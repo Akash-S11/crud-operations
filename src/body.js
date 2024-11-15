450fd904fb1c1bad7b9aef15bc4158785f2d4ea6
Akash S <akashs1132000@gmail.com>
2024-11-15 20:27:06 +0530

    Inital config

---
+++ src/body.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_URL = "https://jsonplaceholder.typicode.com/posts"; 
+
+const Body = () => {
+  return (
+    <div>
+        Body
+    </div>
+  )
+}
+
+export default Body;
